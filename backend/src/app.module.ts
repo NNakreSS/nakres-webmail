134d83c8dc32c2cfa8408414ddc1e50b494f4ad9
NNakreSS <serkanatmaca15@gmail.com>
2025-01-26 23:48:12 +0300

    feat: added configModule & MailModule & RedisModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MailModule } from './mail/mail.module';
+import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from './shared/redis/redis.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MailModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+    RedisModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
